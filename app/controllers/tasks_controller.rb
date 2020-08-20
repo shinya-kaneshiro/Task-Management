5bde9e35ea69d2c781e77a6d28beb91aafe06f6a
shinya-kaneshiro <armageddn1@gmail.com>
2020-08-20 11:05:04 +0000

    C6 sippai_igai_urlmondaikaiketu

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -2,15 +2,14 @@ class TasksController < ApplicationController
   before_action :set_user_id
   
   def new
+    @user = User.find(params[:user_id])
     @task = Task.new
   end
   
   def create
-    task = Task.new
-    task.name = params[:name]
-    task.description = params[:description]
-    task.user_id = params[:user_id]
-    if task.save
+    @task = Task.new(task_params)
+    @task.user_id = params[:user_id]
+    if @task.save
       flash[:success] = "タスクを新規作成しました。"
       redirect_to user_tasks_path
     else
@@ -22,9 +21,9 @@ class TasksController < ApplicationController
     @tasks = Task.all
   end
   
-  # private
+  private
     
-  #   def task_params
-  #     params.require(:task).permit(:name, :description)
-  #   end
+    def task_params
+      params.require(:task).permit(:name, :description)
+    end
 end
