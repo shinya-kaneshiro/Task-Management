049002b91a8470d3059e019ebcd491564f5edd13
shinya-kaneshiro <armageddn1@gmail.com>
2020-08-23 18:02:56 +0000

    Finish D7(D_all_finish)

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -3,7 +3,7 @@ class TasksController < ApplicationController
   
 
   def index
-    @tasks = Task.all
+    @tasks = Task.where(user_id: current_user.id)
   end
   
   def create
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,47 @@
 # coding: utf-8
 
 User.create!(name: "Sample User",
-             email: "sample@email.com",
-             password: "password",
-             password_confirmation: "password",
-             admin: true)
+            email: "sample@email.com",
+            password: "password",
+            password_confirmation: "password",
+            admin: true)
 
 99.times do |n|
   name  = Faker::Name.name
   email = "sample-#{n+1}@email.com"
   password = "password"
   User.create!(name: name,
-               email: email,
-               password: password,
-               password_confirmation: password)
+              email: email,
+              password: password,
+              password_confirmation: password)
+end
+
+
+50.times do |n|
+  name  = "タスク#{n+1}"
+  description = "タスク#{n+1}の詳細"
+  user_id = 1
+  Task.create!(name: name,
+               description: description,
+               user_id: user_id)
+end
+
+
+50.times do |n|
+  name  = "タスク#{n+1}"
+  description = "タスク#{n+1}の詳細"
+  user_id = 2
+  Task.create!(name: name,
+              description: description,
+              user_id: user_id)
+end
+
+
+50.times do |n|
+  name  = "タスク#{n+1}"
+  description = "タスク#{n+1}の詳細"
+  user_id = 3
+  Task.create!(name: name,
+              description: description,
+              user_id: user_id)
 end
